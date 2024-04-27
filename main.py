5de9e7da666c103162716ac4de197d4624824c47
Your Name <you@example.com>
2024-04-27 12:45:46 +0300

    pass_to_manager fix2

--- main.py
+++ main.py
@@ -82,6 +82,7 @@ threads = {}
 
 # function to pass delivery order info to a manager
 def pass_to_manager(location, contact, order, comment=None):
+    print(type(location))
     order_info = f"Contact: {contact}\nOrder: {order}\nDestination: {location}\nComment: {comment}"
     # this info can be redirected to telegram chat with Manager
     bot.send_message(448272985, order_info)
